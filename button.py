eb253b6c53c70f17932541ee8375074328c493d3
ReneSambou <sambourene9@gmail.com>
2023-08-25 14:30:15 +0100

    added button file

---
+++ button.py
@@ -0,0 +1,14 @@
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
+
+led = LED(17)
+button = Button(2)
+
+while True:
+    button.when_pressed = led.on
+    sleep(2)
+    button.when_pressed = led.off
+    sleep(2)
+
+pause()
